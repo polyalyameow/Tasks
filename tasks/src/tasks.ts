c387bec210badceb0e90eca64ea17e6b1b4f5a97
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-04-10 14:13:55 +0200

    toggle added

--- tasks/src/tasks.ts
+++ tasks/src/tasks.ts
@@ -7,7 +7,14 @@ type Task = {
     isCompleted: boolean;
 }
 
-const tasks: Task[] = [];
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(renderTask);
+
+function loadTasks(): Task[]{
+    const storedTasks = localStorage.getItem('Tasks')
+    return storedTasks ? JSON.parse(storedTasks) : []
+}
 
 taskForm?.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -23,6 +30,8 @@ taskForm?.addEventListener('submit', (event) => {
         renderTask(task)
 
         //update local storage
+        updateStorage();
+
 
         formInput.value='';
         return;
@@ -38,5 +47,33 @@ function addTask(task: Task): void {
 function renderTask(task: Task): void {
     const taskElement = document.createElement('li');
     taskElement.textContent = task.description;
+    
+    const taskCheckBox = document.createElement('input')
+    taskCheckBox.type = 'checkbox'
+    taskCheckBox.checked = task.isCompleted;
+
+    taskElement.appendChild(taskCheckBox);
+
+    
+
+    // toggle checkbox
+    taskCheckBox.addEventListener('change', () => {
+        task.isCompleted = !task.isCompleted;
+        if (task.isCompleted) {
+            taskElement.style.textDecoration = "line-through";
+        } else {
+            taskElement.style.textDecoration = "none";
+        }
+        updateStorage(); 
+    })
+
     taskListElement?.appendChild(taskElement)
+
+    if (task.isCompleted) {
+        taskElement.style.textDecoration = "line-through";
+    }
+}
+
+function updateStorage(): void {
+    localStorage.setItem('Tasks', JSON.stringify(tasks));
 }
