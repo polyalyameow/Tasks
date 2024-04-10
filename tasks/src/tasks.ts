e39a5f9581446c069d0c4e7e3a909e0d9105b82b
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-04-10 13:55:11 +0200

    create task-feature created

--- tasks/src/tasks.ts
+++ tasks/src/tasks.ts
@@ -1,1 +1,42 @@
-console.log("typescript");
+const taskForm = document.querySelector<HTMLFormElement>('.form');
+const formInput = document.querySelector<HTMLInputElement>('.form-input');
+const taskListElement =  document.querySelector<HTMLUListElement>('.list');
+
+type Task = {
+    description: string;
+    isCompleted: boolean;
+}
+
+const tasks: Task[] = [];
+
+taskForm?.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskDescription = formInput?.value
+    if(taskDescription) {
+        const task: Task ={
+            description: taskDescription,
+            isCompleted: false
+        }
+        addTask(task)
+
+        //render tasks
+        renderTask(task)
+
+        //update local storage
+
+        formInput.value='';
+        return;
+
+    } alert ('Please enter a task description');
+})
+
+function addTask(task: Task): void {
+    tasks.push(task)
+    console.log(tasks)
+}
+
+function renderTask(task: Task): void {
+    const taskElement = document.createElement('li');
+    taskElement.textContent = task.description;
+    taskListElement?.appendChild(taskElement)
+}
